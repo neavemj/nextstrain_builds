a184a6681f1ee90fcc91fccc1c76e9ca63dbe348
neavemj <matthewjneave1@gmail.com>
2018-12-19 16:28:23 +1100

    added lat long checks, etc

--- BTV/data/preliminary_data/NCBI/add_gb_to_BTV.py
+++ BTV/data/preliminary_data/NCBI/add_gb_to_BTV.py
@@ -6,6 +6,7 @@
 import sys
 import argparse
 from Bio import SeqIO
+from datetime import datetime
 
 parser = argparse.ArgumentParser("update a Nextstrain build with new NCBI sequences\n")
 
@@ -14,9 +15,9 @@ parser.add_argument('-g', '--genbank_files', type = str,
 parser.add_argument('-l', '--current_lat_longs', type = str,
         nargs = "?", help = "the lat_long file for the existing build (new lat longs will be added if required)")
 parser.add_argument('-f', '--current_fastas', type = str,
-        nargs = "?", help = "the fasta files for the existing build (new sequences will be added)")
+        nargs = "+", help = "the fasta files for the existing build (space separated)")
 parser.add_argument('-m', '--current_metas', type = str,
-        nargs = "?", help = "the meta files for the existing build (new meta data will be added)")
+        nargs = "+", help = "the meta files for the existing build (space separated)")
 
 # if no args given, print help and exit
 
@@ -34,8 +35,6 @@ if args.genbank_files is None or \
           "** a file containing wanted accession numbers is required\n")
     parser.print_help(sys.stderr)
     sys.exit(1)
-
-acc_handle = args.acc_file
 
 # function to parse downloaded genbank records
 
@@ -55,6 +54,7 @@ def parse_genbank(fl_name):
         except KeyError:
             print("{} not found in record {}.".format(req, fl_name))
             print("This is a required field - record aborted. \n")
+            return(None)
 
     for opt in optional_qualifiers:
         try:
@@ -62,14 +62,71 @@ def parse_genbank(fl_name):
             record_dict[opt] = info
         except KeyError:
             print("{} not found in record {}.".format(req, fl_name))
-            print("This is an optional - record will be written with 'unknown' in this field. \n")
+            print("This is optional - record will be written with 'Unknown' in this field. \n")
             record_dict[opt] = "Unknown"
 
-    record_dict["authors"] = ",".join(seq_record.annotations['references'][0].authors.split(",")[0:3]) + " et al"
+    record_dict["authors"] = ",".join(seq_record.annotations['references'][0].authors.split(",")[0:2]) + " et al"
     record_dict["title"] = seq_record.annotations['references'][0].title
     record_dict["journal"] = seq_record.annotations['references'][0].journal
 
     return(record_dict)
 
-feat = parse_genbank(sys.argv[1])
-print(feat)
+# first, extract all info from the current lat long file and add to a dict
+# then can check if new lat longs need to be added
+
+current_lat_longs = {}
+
+with open(args.current_lat_longs) as f:
+    for line in f:
+        line = line.strip()
+        cols = line.split("\t")
+        deme = cols[0]
+        place = cols[1]
+        lat_longs = (round(float(cols[2]), 1), round(float(cols[3]), 1))
+        current_lat_longs[lat_longs] = {"deme": deme, "place": place}
+
+# some bits of info in the genbank require some additional processing
+# also will check if the lat longs already exist
+
+def additional_processing(raw_record_dict):
+    if raw_record_dict["country"] != "Unknown":
+        country = raw_record_dict["country"].split(":")[0]
+        state = raw_record_dict["country"].split(":")[1].split(",")[1].strip()
+        town = raw_record_dict["country"].split(":")[1].split(",")[0].strip()
+    else:
+        state, town = "Unknown"
+
+    raw_record_dict["country"] = country
+    raw_record_dict["state"] = state
+    raw_record_dict["town"] = town
+
+    # also want to convert the date properly
+    # useful info available here:
+    # https://docs.python.org/2/library/datetime.html#strftime-and-strptime-behavior
+    date = raw_record_dict["collection_date"]
+    python_date = datetime.strptime(date, "%b-%Y")
+    formatted_date = str(python_date.year) + "-" + str(python_date.month) + "-XX"
+    raw_record_dict["collection_date"] = formatted_date
+
+    # check if the lat longs are in the current lat long file (rounded to 1 decimal place)
+    lat = -round(float(raw_record_dict["lat_lon"].split(" ")[0]), 1)
+    long = round(float(raw_record_dict["lat_lon"].split(" ")[2]), 1)
+
+    if (lat, long) in current_lat_longs:
+        print(lat, long)
+    else:
+        print("lat longs not found")
+        print(lat, long)
+        print(current_lat_longs)
+
+    return(raw_record_dict)
+
+# extract all required info from the genbank files
+
+genbank_dict = {}
+
+for genbank_file in args.genbank_files:
+    original_dict = parse_genbank(genbank_file)
+    genbank_dict[genbank_file] = additional_processing(original_dict)
+
+print(genbank_dict)
