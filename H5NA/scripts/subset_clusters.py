a95e231c8d9962c2a67162ff676ee1fe59f6bf81
neavemj <matthewjneave1@gmail.com>
2019-04-30 14:26:39 +1000

    script to subset dataset based on similarity and meta

--- H5NA/scripts/subset_clusters.py
+++ H5NA/scripts/subset_clusters.py
@@ -48,7 +48,7 @@ if args.fasta is None or \
 meta_dict = {}
 
 with open(args.meta) as fl:
-    meta_dict["header"] = next(fl)
+    meta_header = next(fl)
     for line in fl:
         line = line.strip()
         cols = line.split("\t")
@@ -83,34 +83,45 @@ with open(args.clusters) as fl:
 
             strain_to_cluster[strain] = cluster
 
-print("Total number of clusters: {}".format(len(cluster_to_strain)))
-print("Total number of strains: {}".format(len(strain_to_cluster)) + "\n")
 
 # now go through fasta and check for duplicates
 
+total_strains = 0
 records_to_write = []
+record_ids_to_write = []
 strains_to_write = set()
 meta_to_write = {}
+duplicate_ids = 0
+duplicate_id_list = []
 singleton_clusters = 0
 multiple_clusters = 0
 strains_in_multiple_clusters_written = 0
+dropped_strains = 0
 processed_clusters = set()
 
 with open(args.fasta) as fl:
     for record in SeqIO.parse(fl, "fasta"):
+        total_strains += 1
         record_id = record.id
         record_cluster = strain_to_cluster[record_id]
         # records that are the single member of a cluster should be written
         # these are not similar to any other strain
         if len(cluster_to_strain[record_cluster]) == 1:
             singleton_clusters += 1
-            records_to_write.append(record)
-            meta_to_write[record_id] = meta_dict[record_id]["line"]
+            if record_id not in record_ids_to_write:
+                records_to_write.append(record)
+                record_ids_to_write.append(record_id)
+                meta_to_write[record_id] = meta_dict[record_id]["line"]
+            else:
+                duplicate_ids += 1
+                duplicate_id_list.append(record_id)
         else:
             # if a cluster has already been processed, several strains might need to be
             # written independantly of the below code
             if record_id in strains_to_write:
                 records_to_write.append(record)
+                record_ids_to_write.append(record_id)
+                meta_to_write[record_id] = meta_dict[record_id]["line"]
             # if the strains in a cluster have alread been checked, skip remaining code
             if record_cluster in processed_clusters:
                 continue
@@ -120,35 +131,69 @@ with open(args.fasta) as fl:
                 # could change this to be a bit smarter
                 processed_clusters.add(record_cluster)
                 records_to_write.append(record)
+                record_ids_to_write.append(record_id)
                 meta_to_write[record_id] = meta_dict[record_id]["line"]
                 strains_in_multiple_clusters_written += 1
-                location = meta_dict[record_id]["location"]
-                country = meta_dict[record_id]["country"]
-                date = meta_dict[record_id]["date"]
-                host = meta_dict[record_id]["host"]
+                # i'll make sets of the meta data fields
+                # then I can add to these as each isolate goes through the loop
+                # this ensures that isolates are checked against all e.g. countries present
+                location_set = {meta_dict[record_id]["location"]}
+                country_set = {meta_dict[record_id]["country"]}
+                date_set = {meta_dict[record_id]["date"]}
+                host_set = {meta_dict[record_id]["host"]}
                 # now check if we want to write any other records in this cluster
                 for strain in cluster_to_strain[record_cluster]:
                     multiple_clusters += 1
                     # dont loop through the representaive strain above
                     if strain == record_id: continue
                     # use representative strain info to check against other strains
-                    if meta_dict[strain]["location"] == location and \
-                       meta_dict[strain]["country"] == country and \
-                       meta_dict[strain]["date"] == date and \
-                       meta_dict[strain]["host"] != host:
+                    # if any of these fields are different, the stain will be kept
+                    if any([ \
+                       #meta_dict[strain]["location"] not in location_set, \
+                       #meta_dict[strain]["country"] not in country_set, \
+                       #meta_dict[strain]["host"] not in host_set, \
+                       meta_dict[strain]["date"] not in date_set]):
+
+                       # one of these must be different to the current cluster metadata
+                       # add the new entries
+                       location_set.add(meta_dict[strain]["location"])
+                       country_set.add(meta_dict[strain]["country"])
+                       host_set.add(meta_dict[strain]["host"])
+                       date_set.add(meta_dict[strain]["date"])
+
                        # can't add strain directly to my record list
                        # because not looping through that yet
                        # will add the id to a list instead, then check against this as records come through
-                        strains_in_multiple_clusters_written += 1
-                        strains_to_write.add(strain)
-                        meta_to_write[strain] = meta_dict[strain]["line"]
+                       strains_in_multiple_clusters_written += 1
+                       strains_to_write.add(strain)
+                    else:
+                        dropped_strains += 1
 
-print(len(strains_to_write))
+
+# write list of duplicate ids, and fasta and meta files
+
+with open("duplicate_ids.txt", "w") as fl:
+    for dup in duplicate_id_list: fl.write(dup + "\n")
+
+SeqIO.write(records_to_write, args.fasta_output, "fasta")
+
+with open(args.meta_output, "w") as fl:
+    fl.write(meta_header)
+    for strain_id in meta_to_write:
+        fl.write(meta_to_write[strain_id] + "\n")
+
+
+print("There were {} strains in the original fasta file".format(total_strains))
+print("There were {} clusters formed".format(len(cluster_to_strain)) + "\n")
 
 print("There were {} isolates that were 'singletons' and did not cluster with any other sequence".format(singleton_clusters))
 
 print("There were {} isolates that clustered with others".format(multiple_clusters))
 print("There were {} isolates that clustered with others and were written".format(strains_in_multiple_clusters_written))
+
+print("There were {} isolates that were dropped due to the similarity filtering".format(dropped_strains))
+print("There were {} isolates that were dropped due to duplicate strain ids".format(duplicate_ids))
+print("\t* the ids for these were written to the file 'duplicate_ids.txt'" + "\n")
 
 print("\nThere were {} total records written".format(len(records_to_write)))
 print("There were {} total meta data records written".format(len(meta_to_write)))
